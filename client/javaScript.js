4ed60c149763aa14677f1bd22f5303559c34dbbc
RakottKrumpli04 <baldav04@gmail.com>
2024-02-03 19:34:43 +0100

    szinvaltas 2.0

--- client/javaScript.js
+++ client/javaScript.js
@@ -1,20 +1,40 @@
-
-
-
-
-
 function valt(){
-    var elem = document.getElementById("btn");
+    const elem = document.querySelector(".btn");
+    const navbar = document.querySelector(".navbar");
+    const navlink = document.querySelector(".nav-link");
+    const rows = document.querySelectorAll(".row");
+
+
     if (elem.innerHTML == "Sötét mód"){
+
         elem.innerHTML = "Világos mód";
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
+        btn.classList.replace("btn-outline-dark", "btn-outline-light");
+
+        document.body.style.backgroundColor = "#1E2022";
+
+        navbar.classList.replace("navbar-light", "navbar-dark");
+        navlink.classList.replace("navbar-light", "navbar-dark");
+        document.querySelector(".navbar").style.background = "linear-gradient(270deg, #626a75 0%, #333c47 100%)";
+
+        rows.forEach(row => {row.style.background = "radial-gradient(circle, #626a75 0%, #333c47 100%)";});
+
         localStorage.setItem("btn", "Világos mód");
     }
+
+
     else {
+
         elem.innerHTML = "Sötét mód";
-        document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
+        btn.classList.replace("btn-outline-light", "btn-outline-dark");
+
+        document.body.style.backgroundColor = "white";
+
+        navbar.classList.replace("navbar-dark", "navbar-light");
+        navlink.classList.replace("navbar-dark", "navbar-light");
+        document.querySelector(".navbar").style.background = "linear-gradient(270deg, #bbe9cf 100%, #9ba49e 0%)";
+
+        rows.forEach(row => {row.style.background = "radial-gradient(circle, #bbe9cf 100%, #9ba49e 0%)";});
+
         localStorage.setItem("btn", "Sötét mód");
     } 
 }
